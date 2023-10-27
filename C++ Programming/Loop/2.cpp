1b827b33327b91162ce6d12658e9bb913b55e2e9
Siddharth Chaturvedi <114088741+smilingsiddd@users.noreply.github.com>
2023-10-27 11:57:23 +0530

    Add files via upload

---
+++ C++ Programming/Loop/2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter a number to print Hello n times: ";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        cout << "Hello World"<<endl;
+    }
+
+    return 0;
+}
